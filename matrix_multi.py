856d34a8b57827f98b9344b4ea52435d98a4856d
Nitesh Pathak <niteshp.10@gmail.com>
2023-07-18 22:25:39 +0530

    updated code

--- matrix_multi.py
+++ matrix_multi.py
@@ -1,11 +1,5 @@
 import numpy as np
-a = [[2,3,4],[1,2,3],[6,3,5],[9,2,1]]
-b=[[4,5],[1,8],[0,5]]
-c = [[0]*len(b[0]) for i in range(len(a))]
-for i in range(len(a)):
-    for j in range(len(b[0])):
-        for k in range(len(b)):
-            c[i][j] += a[i][k]*b[k][j]
-print(c)
+a = [[2, 3, 4], [1, 2, 3], [6, 3, 5], [9, 2, 1]]
+b = [[4, 5], [1, 8], [0, 5]]
 
-print(np.dot(a,b))
+print(np.dot(a, b))
